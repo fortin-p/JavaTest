57b00b836e76aa99c95962fff9bb6be475a8b1fd
pierre-elie <fortin.p@hotmail.fr>
2021-06-28 10:44:46 +0200

    Initial commit

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,17 @@
+public class Calculator {
+    public int Sum(int a, int b) {
+        return a + b;
+    }
+
+    public int Sub(int a, int b) {
+        return a - b;
+    }
+
+    public int Divide(int a, int b) {
+        return a / b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+}
---
+++ src/test/java/TestCalculator.java
@@ -0,0 +1,6 @@
+public class TestCalculator {
+
+    public void TestSum(){
+        
+    }
+}
